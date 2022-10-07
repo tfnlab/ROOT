c2f36b388ec80555e7882431e3c981bda2feb9ed
tfnLAB <tfnlab@gmail.com>
2022-10-07 02:30:00 -0700

    Add design file

--- game/Build/game.asm.framework.js
+++ game/Build/game.asm.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d71aeca194bdbb8b23006ec2f84e3b8607134b76643f90dc719f6ef09fbf81b6
+oid sha256:1e45c7ab73b1d83203b32cc4d48012a6b625294ddd0b29ba9d82327db792dc8e
 size 485303
