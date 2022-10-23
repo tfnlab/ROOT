05b42a9470d2b9abbc4c956b8e4f718be629ad8c
tfnLAB <tfnlab@gmail.com>
2022-10-23 15:05:54 -0700

    update game

---
+++ XXXCity/Build/XXXCity.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3dd8287b79bccf25d882dba50849b2725bc5f6d8b391bc1ad3937d65217718b6
+size 16553
---
+++ XXXCity/network.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f7fc14f5cab28529052543e77a4340b46dfc43f60a3e38e7c222e19b38904b1
+size 508
